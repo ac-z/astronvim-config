6d782565abbde7657edf5c6b996c033f4594862c
Amber Connelly <113668892+ac-z@users.noreply.github.com>
2024-10-02 13:03:49 -0700

    Use trouble for diagnostics instead of the default.

--- lua/community.lua
+++ lua/community.lua
@@ -6,4 +6,5 @@ return {
   { import = "astrocommunity.pack.godot" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.completion.codeium-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
 }
